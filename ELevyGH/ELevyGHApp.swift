6ccd7c30a89516d1074bf910c8b386da7779977b
davidAg9 <shane_alpha9@outlook.com>
2022-04-19 19:14:43 +0000

    Initial Commit

---
+++ ELevyGH/ELevyGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELevyGHApp.swift
+//  ELevyGH
+//
+//  Created by Dave Agyakwa on 19/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct ELevyGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
